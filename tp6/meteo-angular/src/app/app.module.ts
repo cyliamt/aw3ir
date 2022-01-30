d3fc171db7aa0f4445b8b52914d586c89249d615
cyliamt <86358723+cyliamt@users.noreply.github.com>
2022-01-30 22:33:51 +0100

    tp6

--- tp6/meteo-angular/src/app/app.module.ts
+++ tp6/meteo-angular/src/app/app.module.ts
@@ -35,8 +35,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
-    )
-    
+    ) 
   ],
   providers: [],
   bootstrap: [AppComponent]
